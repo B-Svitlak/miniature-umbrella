308216854fc7ad6b179c231c553f10ea1aa6180a
bogda <test@ua>
2023-12-16 20:37:43 +0200

    Add class 'Test'

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print("hello")
+class Test:
+    def __init__(self):
+        self.a = 1
+        self.b = 2
+
+    def add(self):
+        return self.a + self.b
+
+    def sub(self):
+        return self.a - self.b
+
+
+if __name__ == '__main__':
+    t = Test()
+    print(t.add())
+    print(t.sub())
